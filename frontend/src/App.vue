c6c6208a602336a61ae3b987d6e10194f9592fce
lnngan1209 <sakamoto12416@gmail.com>
2024-04-21 10:05:46 +0700

    project

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div class="container-fluid mt-3">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "App",
+
+  }
+</script>
+
+<style>
+
+</style>
